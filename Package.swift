6b72f0d68433e004f32b2bae1cff83b06eab4d1a
Steven Roebert <stevenroebert@gmail.com>
2018-04-22 09:10:12 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftLIFX",
+    products: [
+        .library(name: "SwiftLIFX", targets: ["SwiftLIFX"]),
+    ],
     dependencies: [
         // Event-driven network application framework for high performance protocol servers & clients, non-blocking.
         .package(url: "https://github.com/apple/swift-nio.git", from: "1.3.1"),
